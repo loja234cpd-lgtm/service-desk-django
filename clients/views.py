dd5505ed3dc2b343f9990ee9dabc82f86061da90
Eduardo Monteiro <eduowzinhow@gmail.com>
2026-01-06 13:58:38 -0300

    Add Client API with Django Rest Framework

--- clients/views.py
+++ clients/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAdminUser
+from .models import Client
+from .serializers import ClientSerializer
 
-# Create your views here.
+class ClientViewSet(ModelViewSet):
+    queryset = Client.objects.all()
+    serializer_class = ClientSerializer
+    permission_classes = [IsAdminUser]
